833c1b16e6461061bc51a0a9ddb9cb96975e5e42
Hamza Ejaz <52288511+hamzaejaz787@users.noreply.github.com>
2021-03-22 20:55:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+//Scroll to Top
+const toTop = document.querySelector(".to-top");
+window.onscroll = function scrollToTop() {
+  if (
+    document.body.scrollTop > 180 ||
+    document.documentElement.scrollTop > 180
+  ) {
+    toTop.style.display = "block";
+  } else {
+    toTop.style.display = "none";
+  }
+};
+
+toTop.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+//Nav Slide
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-items");
